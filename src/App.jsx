f321a1f89d766c1cc2b7603d93acf0872dca0c2e
Fuadseid <fuaddbus@gmail.com>
2024-12-01 23:21:23 +0300

    adding fake  json server and fetch from it the question

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,33 @@
 import Header from './Header';
 import Mainn from './Mainn';
+import { useReducer } from "react";
+import { useEffect } from "react";
+
+const initialstate = {
+  questions: [],
+  status: "loading",
+};
+function reducer(state, action) {
+  switch (action.type) {
+    case "displayData":
+      return {
+        ...state,
+        questions: action.payload,
+        status: "ready",
+      };
+    default:
+      throw new Error("Error");
+  }
+}
 function App(){
+  const [state, dispatch] = useReducer(reducer, initialstate);
+
+  useEffect(function () {
+    fetch("http://localhost:8000/questions")
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: "displayData", payload: data }))
+      .catch((err) => console.error("Error"));
+  }, []);
   return(
     <div className='app'>
 <Header/>
